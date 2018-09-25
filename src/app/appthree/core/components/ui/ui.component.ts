af346948b6824f113469679b5eb56b53f2ade246
deepali-arvind <deepali-arvind>
2018-09-25 15:48:57 +0000

    First Commit

---
+++ src/app/appthree/core/components/ui/ui.component.ts
@@ -0,0 +1,28 @@
+/**
+ * Created by: deepali-test-new deepali-test-meta
+ * Date: 25/09/2018
+ * Organization: Individual	
+ */
+import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+@Component(
+{
+	selector: 'ui',
+	templateUrl: 'ui.component.html'
+})
+export class UiComponent implements OnInit
+{
+	
+	
+	
+	constructor( private router: Router)
+	{
+		
+	}
+	ngOnInit()
+	{
+	}
+	
+	
+}
+
---
+++ src/app/appthree/core/core.route.ts
@@ -0,0 +1,9 @@
+
+import {Routes } from '@angular/router';
+import { UiComponent } from './components/ui/ui.component';
+export const ROUTES: Routes = [
+{
+	    path: 'ui', component: UiComponent
+},
+];
+
